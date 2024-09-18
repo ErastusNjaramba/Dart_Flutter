d06638ee7929697101fc9a2a2b9fa360a967ad2f
ErastusNjaramba <erastus11njaramba@gmail.com>
2024-09-18 12:09:31 +0300

    Add dart files

---
+++ numbers.dart
@@ -0,0 +1,19 @@
+void main() {
+  int num1 = 43;
+
+  //double decimal
+
+  int num2 = 2;
+  
+  var addition = num1 + num2;
+  print("Addition: $addition");
+
+  var subtraction = num1 - num2;
+  print("Subtraction: $subtraction");
+  
+  var multiplication = num1 * num2;
+  print("Multiplication: $multiplication");
+
+  var division = num1 / num2;
+  print("Division: $division");
+}
---
+++ variables.dart
@@ -0,0 +1,17 @@
+//variables in dart
+//variables are storages location
+void main (){
+  var name = "Erastus Kimani";
+  var age = 30;
+  var place = "Laini saba";
+  var hobbies = "Sleeping and Gaming";
+  var maritalStatus = "Married";
+
+  print(name);
+  print(age);
+  print(place);
+  print(maritalStatus);
+  
+  print("My name is $name I am $age years old I leave in $place My hobbies are $hobbies I have $maritalStatus");
+
+}
